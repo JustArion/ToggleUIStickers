2515c736f4b09020a76bc7061380a87cacc5f521
arion <53127594+Arion-Kun@users.noreply.github.com>
2021-06-22 19:30:52 +0200

    Initial Commit

---
+++ Start.cs
@@ -0,0 +1,63 @@
+﻿extern alias MelonLoader3;
+using System;
+using Dawn.Stickers;
+using MelonLoader3::MelonLoader;
+
+[assembly: MelonInfo(typeof(Start), Start.MODID, "1.0.0",  "arion#1223", "https://github.com/Arion-Kun/ToggleUIStickers/releases/latest")]
+[assembly: MelonColor(ConsoleColor.DarkCyan)]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+
+namespace Dawn.Stickers
+{
+    using System.Collections;
+    using UnityEngine;
+
+    internal sealed class Start : MelonMod
+    {
+        internal const string MODID = "ToggleUIStickers";
+        #region Preferences
+
+        static MelonPreferences_Entry<bool> _UseVRCPlusStickers;
+        internal static bool UseVRCPlusStickers => _UseVRCPlusStickers.Value;
+        static MelonPreferences_Entry<bool> _UseBuildInfoStickers;
+        internal static bool UseBuildInfoStickers => _UseBuildInfoStickers.Value;
+        static MelonPreferences_Entry<bool> _UseEarlyAccessStickers;
+        internal static bool UseEarlyAccessStickers => _UseEarlyAccessStickers.Value;
+        static MelonPreferences_Entry<bool> _UseNewStickers;
+        internal static bool UseNewStickers => _UseNewStickers.Value;
+        #endregion
+        public override void OnApplicationStart()
+        {
+            RegisterSettings();
+            Toggler.RegisterStateListeners();
+        }
+
+        static void RegisterSettings()
+        {
+            var modCategory = MelonPreferences.CreateCategory(MODID, "UI Stickers");
+            
+            
+            _UseVRCPlusStickers = modCategory.CreateEntry("VRCPlus", false, "VRC+") as MelonPreferences_Entry<bool>;
+            _UseBuildInfoStickers = modCategory.CreateEntry("BuildInfo", false, "Build Info") as MelonPreferences_Entry<bool>;
+            _UseEarlyAccessStickers = modCategory.CreateEntry("EarlyAccess", false, "Early Access") as MelonPreferences_Entry<bool>;
+            _UseNewStickers = modCategory.CreateEntry("New", false, "New") as MelonPreferences_Entry<bool>;
+        }
+
+        //Thanks Bono! <3
+        [Credit("DDAkebono", "https://github.com/ddakebono/BTKSAImmersiveHud/blob/0e1e169288abc45ecb5e92b143f90a657d921d16/BTKSAImmersiveHud.cs#L60")]
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (scenesLoaded is null or > 2) return;
+            scenesLoaded++;
+            if (scenesLoaded != 2) return;
+            scenesLoaded = null;
+            MelonCoroutines.Start(DelayUpdate()); // To allow other mods to effectively copy those objects in their initial states.
+        }
+
+        private static IEnumerator DelayUpdate() { yield return new WaitForSeconds(15f); Toggler.ForceUpdateListeners(); }
+        static byte? scenesLoaded = 0;
+
+        public override void OnPreferencesSaved() { if (scenesLoaded != null) return; Toggler.UpdateListeners(); }
+    }
+}
---
+++ Toggler.cs
@@ -0,0 +1,187 @@
+﻿namespace Dawn.Stickers
+{
+    extern alias MelonLoader3;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using MelonLoader3::MelonLoader;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Utilities;
+
+    internal static class Toggler
+    {
+        private static void ToggleMessage(string array, int count, bool hide) => MelonLogger.Msg(hide ? $"Hid {count} '{array}' Stickers" : $"Showed {count} '{array}' Stickers");
+        internal static void RegisterStateListeners()
+        {
+            VRCPlusStateListener = new PreferencesStateListener(Start.UseVRCPlusStickers, () => {
+                foreach (var Sticker in VRCPlusStickers)
+                {
+                    //Other mods might destroy the obj completely so we remove it.
+                    if (Sticker == null) { cachedVRCPlusStickers = cachedVRCPlusStickers.Where(item => item != Sticker).ToArray(); continue; }
+
+                    var y = Sticker.GetComponent<Image>();
+                    if (y != null) { y.enabled = true; continue; }
+                    
+                    Sticker.SetActive(true);
+                }
+                ToggleMessage("VRC+", VRCPlusStickers.Count(), false);
+            }, () => {
+                foreach (var Sticker in VRCPlusStickers)
+                {
+                    if (Sticker == null) { cachedVRCPlusStickers = cachedVRCPlusStickers.Where(item => item != Sticker).ToArray(); continue; }
+
+                    var y = Sticker.GetComponent<Image>();
+                    if (y != null) { y.enabled = false; continue; }
+                    
+                    Sticker.SetActive(false);
+                }
+                ToggleMessage("VRC+", VRCPlusStickers.Count(), true);
+            });
+            BuildInfoStateListener = new PreferencesStateListener(Start.UseBuildInfoStickers, () => {
+                foreach (var Sticker in BuildInfoStickers)
+                {
+                    if (Sticker == null) { cachedBuildInfoStickers = cachedBuildInfoStickers.Where(item => item != Sticker).ToArray(); continue; }
+                    
+                    var y = Sticker.GetComponent<Text>();
+                    if (y != null) y.enabled = true;
+                }
+                ToggleMessage("BuildInfo", BuildInfoStickers.Count(), false);
+            }, () => {
+                foreach (var x in BuildInfoStickers)
+                {
+                    if (x == null) { cachedBuildInfoStickers = cachedBuildInfoStickers.Where(item => item != x).ToArray(); continue; }
+                    
+                    var y = x.GetComponent<Text>();
+                    if (y != null) y.enabled = false;
+                }
+                ToggleMessage("BuildInfo", BuildInfoStickers.Count(), true);
+            });
+            EarlyAccessStateListener = new PreferencesStateListener(Start.UseEarlyAccessStickers, () => {
+                foreach (var Sticker in EarlyAccessStickers)
+                {
+                    if (Sticker == null) { cachedEarlyAccessStickers = cachedEarlyAccessStickers.Where(item => item != Sticker).ToArray(); continue; }
+                    
+                    var y = Sticker.GetComponent<Text>();
+                    if (y != null)
+                    {
+                        y.enabled = true;
+                        continue;
+                    }
+                    var z = Sticker.GetComponent<Image>();
+                    if (z != null)
+                    {
+                        z.enabled = true;
+                    }
+                }
+                ToggleMessage("EarlyAccess", EarlyAccessStickers.Count(), false);
+            }, () => {
+                foreach (var Sticker in EarlyAccessStickers)
+                {
+                    if (Sticker == null) { cachedEarlyAccessStickers = cachedEarlyAccessStickers.Where(item => item != Sticker).ToArray(); continue; }
+                    
+                    var y = Sticker.GetComponent<Text>();
+                    if (y != null)
+                    {
+                        y.enabled = false;
+                        continue;
+                    }
+                    var z = Sticker.GetComponent<Image>();
+                    if (z != null)
+                    {
+                        z.enabled = false;
+                    }
+                }
+                ToggleMessage("EarlyAccess", EarlyAccessStickers.Count(), true);
+            });
+            NewStateListener = new PreferencesStateListener(Start.UseNewStickers, () => {
+                foreach (var Sticker in NewStickers)
+                {
+                    if (Sticker == null) { cachedNewStickers = cachedNewStickers.Where(item => item != Sticker).ToArray(); continue; }
+
+                    var y = Sticker.GetComponent<Image>();
+                    if (y != null)
+                    {
+                        y.enabled = true;
+                    }
+                }
+                ToggleMessage("New", NewStickers.Count(), false);
+            }, () => {
+                foreach (var Sticker in NewStickers)
+                {
+                    if (Sticker == null) { cachedNewStickers = cachedNewStickers.Where(item => item != Sticker).ToArray(); continue; }
+
+                    var y = Sticker.GetComponent<Image>();
+                    if (y != null)
+                    {
+                        y.enabled = false;
+                    }
+                }
+                ToggleMessage("New", NewStickers.Count(), true);
+            });
+        }
+        
+        internal static void UpdateListeners()
+        {
+            VRCPlusStateListener.Update(Start.UseVRCPlusStickers);
+            BuildInfoStateListener.Update(Start.UseBuildInfoStickers);
+            EarlyAccessStateListener.Update(Start.UseEarlyAccessStickers);
+            NewStateListener.Update(Start.UseNewStickers);
+        }
+        internal static void ForceUpdateListeners()
+        {
+            VRCPlusStateListener.ForceUpdate(Start.UseVRCPlusStickers);
+            BuildInfoStateListener.ForceUpdate(Start.UseBuildInfoStickers);
+            EarlyAccessStateListener.ForceUpdate(Start.UseEarlyAccessStickers);
+            NewStateListener.ForceUpdate(Start.UseNewStickers);
+        }
+
+        private static PreferencesStateListener VRCPlusStateListener;
+        private static PreferencesStateListener BuildInfoStateListener;
+        private static PreferencesStateListener EarlyAccessStateListener;
+        private static PreferencesStateListener NewStateListener;
+        
+        
+
+        static GameObject[] cachedVRCPlusStickers;
+        static IEnumerable<GameObject> VRCPlusStickers=> cachedVRCPlusStickers ??= Resources.FindObjectsOfTypeAll<GameObject>().Where(o => o.name is "VRC+" or "Icon_VRC+").ToArray();
+        
+        static GameObject[] cachedBuildInfoStickers;
+        static IEnumerable<GameObject> BuildInfoStickers
+        {
+            get
+            {
+                if (cachedBuildInfoStickers != null) return cachedBuildInfoStickers;
+                var x = new List<GameObject>();
+                var QMBuildNumber = GameObject.Find("UserInterface/QuickMenu/QuickMenu_NewElements/_InfoBar/BuildNumText");
+                var MainMenuVersionText = GameObject.Find("UserInterface/MenuContent/Screens").transform.Find("Settings/TitlePanel/VersionText").gameObject;
+                    
+                if (QMBuildNumber != null) x.Add(QMBuildNumber);
+                if (MainMenuVersionText != null) x.Add(MainMenuVersionText);
+                return cachedBuildInfoStickers = x.ToArray();
+            }
+        }
+
+        static GameObject[] cachedEarlyAccessStickers;
+        static IEnumerable<GameObject> EarlyAccessStickers
+        {
+            get
+            {
+                if (cachedEarlyAccessStickers != null) return cachedEarlyAccessStickers;
+                var x = new List<GameObject>();
+                var QMEarlyAccess = GameObject.Find("UserInterface/QuickMenu/QuickMenu_NewElements/_InfoBar/EarlyAccessText");
+                var MainMenuEarlyAccess = GameObject.Find("UserInterface/MenuContent/").transform.Find("Backdrop/Backdrop/Image").gameObject;
+                
+                if (QMEarlyAccess != null) x.Add(QMEarlyAccess);
+                if (MainMenuEarlyAccess != null) x.Add(MainMenuEarlyAccess);
+                return cachedEarlyAccessStickers = x.ToArray();
+            }
+        }
+        static GameObject[] cachedNewStickers;
+        private static IEnumerable<GameObject> NewStickers => cachedNewStickers ??= Resources.FindObjectsOfTypeAll<GameObject>()
+            .Where(o => o.name is "Image_NEW").ToArray();
+
+
+    }
+}
